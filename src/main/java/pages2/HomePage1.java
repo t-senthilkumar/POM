3def685e350c963289f01a8d53085d851a89d996
owner <owner@home>
2018-03-04 09:07:54 +0530

    1st commit

---
+++ src/main/java/pages2/HomePage1.java
@@ -0,0 +1,16 @@
+package pages2;
+
+import org.openqa.selenium.support.PageFactory;
+
+import wdMethods.ProjectMethods;
+
+public class HomePage1 extends ProjectMethods{
+
+	public HomePage1() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/pages2/LoginPage1.java
@@ -0,0 +1,14 @@
+package pages2;
+
+import org.openqa.selenium.support.PageFactory;
+
+import wdMethods.ProjectMethods;
+
+public class LoginPage1 extends ProjectMethods{
+
+	public LoginPage1() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+}
